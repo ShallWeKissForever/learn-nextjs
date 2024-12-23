c4977e15182af54ff3bfbebd00ff2591b1626faa
safedelusion <1661456488@qq.com>
2024-12-23 20:19:38 +0800

    Remove unused files, fix typos, and update dependencies

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function invoices () {
+export default function invoices() {
   return (
     <div>Invoices Page</div>
   )
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>Fashboard Page</div>
+    <div>Dashboard Page</div>
   )
 }
